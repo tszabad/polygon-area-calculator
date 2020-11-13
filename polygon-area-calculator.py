9e27257420d97beffb79e2464196ce614af18fd8
Tamas Szabados <tszabad@gmail.com>
2020-11-13 21:34:25 +0100

    classes added

--- polygon-area-calculator.py
+++ polygon-area-calculator.py
@@ -5,11 +5,11 @@ class Rectangle:
   
   def set_width(self, width):
     self.width = width
-    return True
+    
 
   def set_height(self, height):
     self.height = height
-    return True
+   
 
   def get_area(self):
     return self.width * self.height
@@ -17,9 +17,14 @@ class Rectangle:
   def get_perimeter(self):
     return 2 * self.width + 2 * self.height
 
-  def get_picture(self):
+  def get_picture(self, rectangle):
     if self.width or self.height > 50:
       return "Too big for picture."
+    else:
+      width = "*" * self.width
+      picture = (width + "\n") * self.height
+
+    return picture
 
   def get_amount_inside():
     pass
@@ -28,5 +33,20 @@ class Rectangle:
 
 
 class Square(Rectangle):
-  def __init__(self, width, height):
-      super().__init__(width, height)
+  def __init__(self, length):
+    super().__init__(length,length)
+
+  def set_side(self, length):
+    self.width = length
+    self.height = length
+
+  def set_width(self, length):
+    self.set_side(length)
+    
+  def set_height(self, length):
+    self.setside(length)
+
+rect = Rectangle(10, 5)
+print(rect.get_area())
+rect.set_height(3)
+print(rect.get_perimeter())
